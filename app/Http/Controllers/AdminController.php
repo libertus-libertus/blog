2857a6268252caea8afdd572e082138b36491261
httpbert <libertuslibert@gmail.com>
2024-04-08 19:55:42 +0700

    finish customize admin temaplates and customize register form

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
+
+class AdminController extends Controller
+{
+
+    // Route logout admin
+    public function destroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    } // End Method
+}
---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,1 @@
+<img src="{{ asset('as_logo/logo.png') }}" alt="" width="150">
