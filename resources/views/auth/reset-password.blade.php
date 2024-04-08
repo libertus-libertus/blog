6bf742e6d45c99e4165ae54850bc5d24579fe88f
httpbert <libertuslibert@gmail.com>
2024-04-08 21:21:21 +0700

    customize reset password

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,39 +1,98 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('password.store') }}">
-        @csrf
+<!doctype html>
+<html lang="en">
 
-        <!-- Password Reset Token -->
-        <input type="hidden" name="token" value="{{ $request->route('token') }}">
+    <head>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <meta charset="utf-8" />
+        <title>Reset Password | PT. Mattaoi Bumi Sikerei</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="PT. Mattaoi Bumi Sikerei" name="description" />
+        <meta content="Libertus" name="author" />
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="{{ asset('as_logo/logo.png') }}">
+
+        <!-- Bootstrap Css -->
+        <link href="{{ asset('backend/assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+        <!-- Icons Css -->
+        <link href="{{ asset('backend/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+        <!-- App Css-->
+        <link href="{{ asset('backend/assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
+
+    </head>
+
+    <body class="auth-body-bg">
+        <div class="bg-overlay"></div>
+        <div class="wrapper-page">
+            <div class="container-fluid p-0">
+                <div class="card">
+                    <div class="card-body">
+
+                        <div class="text-center mt-4">
+                            <div class="mb-3">
+                                <a href="index.html" class="auth-logo">
+                                    <img src="{{ asset('as_logo/favicon.png') }}" height="100" class="logo-dark mx-auto" alt="">
+                                    <img src="{{ asset('as_logo/favicon.png') }}" height="100" class="logo-light mx-auto" alt="">
+                                </a>
+                            </div>
+                        </div>
+
+                        <h4 class="text-muted text-center font-size-18"><b>Reset Password</b></h4>
+
+                        <div class="p-3">
+                            <form class="form-horizontal mt-3" method="POST" action="{{ route('password.store') }}">
+                                @csrf
+
+                                <!-- Password Reset Token -->
+                                <input type="hidden" name="token" value="{{ $request->route('token') }}">
+
+                                <div class="alert alert-info alert-dismissible fade show" role="alert">
+                                Forgot your password here!
+                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                </div>
+
+                                <div class="form-group mb-3">
+                                    <div class="col-xs-12">
+                                        <input class="form-control" id="email" type="email" name="email" required="" placeholder="Email" required autofocus>
+                                    </div>
+                                </div>
+
+                                <div class="form-group mb-3">
+                                    <div class="col-xs-12">
+                                        <input class="form-control" id="password" type="password" name="password" required="" placeholder="New password" required autofocus>
+                                    </div>
+                                </div>
+
+                                <div class="form-group mb-3">
+                                    <div class="col-xs-12">
+                                        <input class="form-control" id="password_confirmation" type="password_confirmation" name="password_confirmation" required="" placeholder="Password confirmation" required autofocus>
+                                    </div>
+                                </div>
+
+                                <div class="form-group pb-2 text-center row mt-3">
+                                    <div class="col-12">
+                                        <button class="btn btn-info w-100 waves-effect waves-light" type="submit">Reset Password</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <!-- end cardbody -->
+                </div>
+                <!-- end card -->
+            </div>
+            <!-- end container -->
         </div>
+        <!-- end -->
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+        <!-- JAVASCRIPT -->
+        <script src="{{ asset('backend/assets/libs/jquery/jquery.min.js') }}"></script>
+        <script src="{{ asset('backend/assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+        <script src="{{ asset('backend/assets/libs/metismenu/metisMenu.min.js') }}"></script>
+        <script src="{{ asset('backend/assets/libs/simplebar/simplebar.min.js') }}"></script>
+        <script src="{{ asset('backend/assets/libs/node-waves/waves.min.js') }}"></script>
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required autocomplete="new-password" />
+        <script src="{{ asset('backend/assets/js/app.js') }}"></script>
 
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Reset Password') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </body>
+</html>
